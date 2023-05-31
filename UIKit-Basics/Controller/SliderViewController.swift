1d96beeaf4383c23252ad6cd917e8c9fb936581f
Carlos Morgado <cmorgadoalarcon@gmail.com>
2023-05-31 14:29:03 +0200

    Preparing Slider-screen

--- UIKit-Basics/Controller/SliderViewController.swift
+++ UIKit-Basics/Controller/SliderViewController.swift
@@ -10,16 +10,35 @@ import UIKit
 class SliderViewController: UIViewController {
 
     // OUTLETS
+    @IBOutlet weak private var generalContentView: UIView!
+    @IBOutlet weak private var sliderMainTitle: UILabel!
     
     // LIFE CYCLE
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // PROPERTIES AND CONFIGURATIONS
-        title = "UISlider"
-        
+        configView()
     }
     
   // ACTIONS
    
+}
+
+// MARK: - EXTENSIONS
+
+private extension SliderViewController {
+    func configView() {
+        title = "UISlider"
+        view.backgroundColor = .basicBackgroundColor
+        generalContentView.backgroundColor = .basicBackgroundColor
+        
+        configSliderMainTitle()
+    }
+    
+    func configSliderMainTitle() {
+        sliderMainTitle.text = "UISlider"
+        sliderMainTitle.textAlignment = .left
+        sliderMainTitle.font = UIFont(name: "SFUIDisplay-Bold", size: 35)
+        sliderMainTitle.textColor = .textColor
+    }
 }
