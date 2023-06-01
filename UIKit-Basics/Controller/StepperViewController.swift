763e94653cc15a627f516912bb042bf1aa26e289
Carlos Morgado <cmorgadoalarcon@gmail.com>
2023-06-01 10:35:03 +0200

    Creating Stepper-screen

--- UIKit-Basics/Controller/StepperViewController.swift
+++ UIKit-Basics/Controller/StepperViewController.swift
@@ -10,16 +10,37 @@ import UIKit
 class StepperViewController: UIViewController {
 
     // OUTLETS
+    @IBOutlet weak private var generalContentView: UIView!
+    @IBOutlet weak private var stepperMainTitle: UILabel!
+    
     
     // LIFE CYCLE
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // PROPERTIES AND CONFIGURATIONS
-        title = "UIStepper"
-        
+        configView()
     }
     
   // ACTIONS
    
+}
+
+
+// MARK: - GENERAL CONFIGURATION EXTENSION
+
+private extension StepperViewController {
+    func configView() {
+        title = "UIStepper"
+        view.backgroundColor = .basicBackgroundColor
+        generalContentView.backgroundColor = .basicBackgroundColor
+        
+        configStepperMainTitle()
+    }
+    
+    func configStepperMainTitle() {
+        stepperMainTitle.text = "UIStepper"
+        stepperMainTitle.textAlignment = .left
+        stepperMainTitle.font = UIFont(name: "SFUIDisplay-Bold", size: 35)
+        stepperMainTitle.textColor = .textColor
+    }
 }
