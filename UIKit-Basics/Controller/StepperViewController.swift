183734edcf3e02457b1ebd96812b9884d713f4ed
Carlos Morgado <cmorgadoalarcon@gmail.com>
2023-06-01 12:09:27 +0200

    Creating Stepper+Slider+PickerView

--- UIKit-Basics/Controller/StepperViewController.swift
+++ UIKit-Basics/Controller/StepperViewController.swift
@@ -12,7 +12,12 @@ class StepperViewController: UIViewController {
     // OUTLETS
     @IBOutlet weak private var generalContentView: UIView!
     @IBOutlet weak private var stepperMainTitle: UILabel!
+    @IBOutlet weak private var stepperExample: UIStepper!
+    @IBOutlet weak private var sliderExample: UISlider!
+    @IBOutlet weak private var pickerViewExample: UIPickerView!
     
+    // VARIABLES
+    private let pickerViewItems = ["Madrid", "Rome", "London", "New York"]
     
     // LIFE CYCLE
     override func viewDidLoad() {
@@ -23,6 +28,7 @@ class StepperViewController: UIViewController {
     
   // ACTIONS
    
+    
 }
 
 
@@ -35,6 +41,13 @@ private extension StepperViewController {
         generalContentView.backgroundColor = .basicBackgroundColor
         
         configStepperMainTitle()
+        
+        configStepperExample()
+        
+        configSliderExample()
+        
+        configPickerViewExample()
+        
     }
     
     func configStepperMainTitle() {
@@ -42,5 +55,58 @@ private extension StepperViewController {
         stepperMainTitle.textAlignment = .left
         stepperMainTitle.font = UIFont(name: "SFUIDisplay-Bold", size: 35)
         stepperMainTitle.textColor = .textColor
+    }
+    
+    func configStepperExample() {
+        stepperExample.minimumValue = 1
+        stepperExample.maximumValue = Double(pickerViewItems.count)
+    }
+    
+    @IBAction func stepperExampleAction(_ sender: Any) {
+        let stepperValue = stepperExample.value
+        sliderExample.value = Float(stepperValue)
+        pickerViewExample.selectRow(Int(stepperValue), inComponent: 0, animated: true)
+        
+    }
+    
+    func configSliderExample() {
+        sliderExample.minimumTrackTintColor = .systemRed
+        sliderExample.maximumTrackTintColor = .systemGray6
+        sliderExample.minimumValue = 1
+        sliderExample.maximumValue = Float(pickerViewItems.count)
+        sliderExample.value = 1
+    }
+    
+    func configPickerViewExample() {
+        pickerViewExample.dataSource = self
+        pickerViewExample.delegate = self
+    }
+}
+
+// MARK: - DATA SOURCE PICKERVIEW EXTENSION
+
+extension StepperViewController: UIPickerViewDataSource {
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return pickerViewItems.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return pickerViewItems[row]
+    }
+}
+
+// MARK: - PICKERVIEW DELEGATE EXTENSION
+
+extension StepperViewController: UIPickerViewDelegate {
+    func pickerView(_ pickerView: UIPickerView, rowHeightForComponent component: Int) -> CGFloat {
+        return 35
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        
     }
 }
