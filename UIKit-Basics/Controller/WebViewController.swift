106de709360166a5306f548dc2019dba00922ff5
Carlos Morgado <cmorgadoalarcon@gmail.com>
2023-06-14 09:47:32 +0200

    WebView-screen V1

--- UIKit-Basics/Controller/WebViewController.swift
+++ UIKit-Basics/Controller/WebViewController.swift
@@ -6,24 +6,45 @@
 //
 
 import UIKit
+import WebKit
 
 class WebViewController: UIViewController {
 
+    // OUTLETS
+    @IBOutlet weak private var generalContentView: UIView!
+    @IBOutlet weak private var webViewMainTitle: UILabel!
+    @IBOutlet weak private var webViewExample: WKWebView!
+    
+    // LIFE CYCLE
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        configView()
     }
+    
+    // ACTIONS
 
+}
 
-    /*
-    // MARK: - Navigation
+// MARK: - EXTENSION
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+private extension WebViewController {
+    func configView() {
+        title = "Web View"
+        view.backgroundColor = .basicBackgroundColor
+        
+        generalContentView.backgroundColor = .basicBackgroundColor
+        
+        configWebViewMainTitle()
+        
+        webViewExample.load(URLRequest(url: URL(string: "https://google.es")!))
     }
-    */
-
+    
+    func configWebViewMainTitle() {
+        webViewMainTitle.text = "Web View"
+        webViewMainTitle.textAlignment = .left
+        webViewMainTitle.font = UIFont(name: "SFUIDisplay-Bold", size: 35)
+        webViewMainTitle.textColor = .textColor
+    }
 }
